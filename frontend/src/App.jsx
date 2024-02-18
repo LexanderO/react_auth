3b7421708a1259fa3878ffedfbaab4ec05b66f15
LexanderoW <alexanderowens1993@gmail.com>
2024-02-18 12:01:45 +0000

    added 404 page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Welcome from "./pages/Welcome";
+import NoPage from "./pages/NoPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Welcome />}></Route>
+          <Route path="*" element={<NoPage />}></Route>
         </Routes>
       </BrowserRouter>
     </>
